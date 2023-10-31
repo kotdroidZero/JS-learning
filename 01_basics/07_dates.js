1f678374bdfb47c6e7fe3b90225d577453eafc32
Pushkar Srivastava <d.errorfound@gmail.com>
2023-10-31 17:56:17 +0530

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,11 @@
+/// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString())
+
+let myDate1 = new Date(2023, 0, 23)
+console.log(myDate1.toString());
