d415246434640375b67cd9643b22ffeb1a4dc519
Pushkar Srivastava <d.errorfound@gmail.com>
2023-10-31 17:28:54 +0530

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,29 @@
+const name = "Pushakr"
+const repoCount = 50
+
+/// Old Way
+console.log(name + repoCount);
+
+/// Using Modern BackTicks and String Interpolation
+
+console.log(`Hello My name is ${name} and My Repo count is ${repoCount}`)
+
+const gameName = new String("Pushakr Srivastava")
+
+console.log(typeof name);
+
+// charAt
+// indexOf(2)
+// substring
+// slice
+// trim
+// replace
+// includes (similar to contains)
+// split
+
+
+const newString = "JaiHanuman"
+
+console.log(newString.substring(6, -3))
+console.log(newString.slice(5, -3))
+
