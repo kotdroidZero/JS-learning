362a9ae5d8d00c89680a6a9503d3075aac363455
Pushkar Srivastava <d.errorfound@gmail.com>
2023-10-31 17:44:00 +0530

    maths and nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,31 @@
+const score = 400;
+const balance = new Number(100.2334)
+
+// console.log(score);
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNumber = 323.25433333
+
+// console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-In'))
+
+
+//=================== Maths==========================
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.6))
+// console.log(Math.floor(4.6))
+// console.log(Math.sqrt(254))
+// console.log(Math.random())
+console.log(Math.random() * 10 + 1)
+console.log(Math.random() * 10 + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min))
