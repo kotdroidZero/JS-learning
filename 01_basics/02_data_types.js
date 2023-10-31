c16a1b135c747479a2745e7339e9c7cbf0e2d667
Pushkar Srivastava <pushkar.srivastva8@appinventiv.com>
2023-10-31 15:19:29 +0530

    data types

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,33 @@
+/// tc39.es  =>> Orginal Docs
+/// mdn ==> JS Docs
+
+"use strict" ;   // A way to tell the JS Engine to trat your JS file as newer version of Java Script
+
+
+// alert(2+2) // We are using node.js not browser
+
+let name = "Pushkar"
+let age = 28
+
+
+//Primitives
+
+/// number
+/// bigint
+/// string=> ""
+/// bolean => true/false
+/// null  => standAlone value
+/// undefined => when value is not defined (not initialized)
+/// symbol => unique (in react)
+
+
+
+// Non-Primitives
+// object
+
+
+/// typeof to get the type of any variables 
+
+console.log(typeof true);
+console.log(typeof null);  /// null is an object
+console.log(typeof undefined); /// undefined is a type
