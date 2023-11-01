99b316b0f1ba8501a5205d2cda8a4c2c82068beb
Pushkar Srivastava <d.errorfound@gmail.com>
2023-11-01 16:46:48 +0530

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,24 @@
+/// Immediately Invoked Function Expressions (IIFE)
+
+
+/// Named IIFI
+(function chai() {
+    console.log("DB Conected");
+})();
+
+
+/// unNamed Named IIFI
+((name) => {
+    console.log(`Hi ${name} , DB Conected Two`);
+})("Pushkar");
+
+// chai()
+
+
+// /// TO Reduce the Global scope pollutions IIFFE is used    (
+// (function a() { console.log("Hi") }
+// )()  /// first () is function body and second is() function execution
+
+
+
+
