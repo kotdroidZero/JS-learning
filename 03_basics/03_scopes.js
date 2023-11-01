ae53b711b783bc0d0622a7244c7eda44d0b1faa6
Pushkar Srivastava <d.errorfound@gmail.com>
2023-11-01 16:06:29 +0530

    scopes

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+if (true) {
+    let a = 10;
+    const b = 20;
+    var c = 30;
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one() {
+
+    const username = "Pushkar"
+
+    function two() {
+        const web = "insta"
+        // console.log(username)
+    }
+
+
+    // console.log(web)
+
+    two();
+}
+
+one();
+
+
+if (true) {
+    const username = "pushkar"
+
+    if (username === "pushkar") {
+        const website = " youtube"
+        // console.log(username + website)
+    }
+
+    // console.log(website);
+}
+
+
+// console.log(username);
+
+
+//============================++++++++++++++++++++++++++++++++++++++++
+console.log(addOne(5));
+function addOne(num) {
+    return num + 1;
+}
+
+
+
+
+
+addTwo(5)
+
+/// function or called expresssion also 
+const addTwo = function (num) {
+    return num + 2
+}
+
+
+/// when function is declared by expression , it needs to be declared before it's use
+/// Hoisting is JavaScript's default behavior of moving all declarations to the top of the current scope (to the top of the current script or the current function).
+
+// ES6
+
